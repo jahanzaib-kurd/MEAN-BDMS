7de82f5bb4263a2301f5a0b8869f6bc86569a9ce
jahanzaib.kurd <jahanzaib.jawaid@gmail.com>
2018-03-02 18:50:20 +0500

    bug fix for angular flash messages version mismatch

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -40,7 +40,7 @@ const appRoutes: Routes = [
     FormsModule,
     HttpModule,
     RouterModule.forRoot(appRoutes),
-    FlashMessagesModule.forRoot()
+    FlashMessagesModule
   ],
   providers: [ValidateService, AuthService],
   bootstrap: [AppComponent]
