f4effb4ee64c05cedba02acc542e5bdc8b77311e
jahanzaib.kurd <jahanzaib.jawaid@gmail.com>
2018-03-05 17:00:57 +0500

    changed User to user

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
