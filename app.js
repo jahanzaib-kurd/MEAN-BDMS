469333fae235eb25957f57ce1a463462a74ba1a9
jahanzaib.kurd <jahanzaib.jawaid@gmail.com>
2018-03-05 17:09:46 +0500

    bug fix

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connection.on('error',(err)=>{
 });
 
 const app = express();
-const port = process.env.port || 8080;
+const port = process.env.PORT || 8080;
 
 const users = require('./routes/users');
 
